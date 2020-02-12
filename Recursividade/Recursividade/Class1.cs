32a83eb0021a815b6e502d9dcceafd29dd633e17
Samuel Costa <39769902+scsoliveira@users.noreply.github.com>
2020-02-12 17:05:41 -0300

    Recursividade ex

---
+++ Recursividade/Recursividade/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recursividade
+{
+    class Contas
+    {
+        public int Pot(int x, int n)
+        {
+            if (n == 0) return 1;
+
+            return x * Pot(x, n - 1);
+        }
+
+        public int Fib(int n)
+        {
+            if (n <= 2) return 1;
+            return Fib(n - 1) + Fib(n - 2);
+        }
+
+        public int Soma(int n)
+        {
+            if (n < 10) return n;
+            return n % 10 + Soma(n / 10);
+        }
+    }
+}
---
+++ Recursividade/Recursividade/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Recursividade
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Contas ct = new Contas();
+
+            //POTENCIA
+            Console.WriteLine(ct.Pot(2, 6));
+            //FIBO
+            Console.WriteLine(ct.Fib(10));
+            //SOMA
+            Console.WriteLine(ct.Soma(1234));
+
+            Console.Read();
+        }
+
+        
+
+
+
+    }
+
+}
